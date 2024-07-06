5e39a6670d27f3d14152ad5d1d6ada887bfe1ae5
Malkoot Khan <engr.mkhan1990@gmail.com>
2024-07-06 15:41:16 +0100

    pseudo code added

--- scheduler.c
+++ scheduler.c
@@ -1,1 +1,82 @@
-/*Basic Scheduler concepts*/
+#include <stdio.h>
+#include <stdbool.h>
+
+#define ENABLE 1
+#define DISABLE 0
+#define REQUIRED_TICKS 100
+#define NUM_TASKS 4
+#define TRUE 1
+
+volatile int tick = 0;
+volatile int context_switch = DISABLE;
+volatile int current_task = 0;
+
+void PendSV_Handler()
+{
+    context_switch = ENABLE;
+}
+
+void SysTick_Handler()
+{
+    tick++;
+    if (tick >= REQUIRED_TICKS)
+    {
+        tick = 0;  // Reset tick
+        trigger_PendSV();
+    }
+}
+
+void trigger_PendSV()
+{
+    SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;  // Set PendSV bit
+}
+
+void save_context()
+{
+    // Code to save the current task's context
+}
+
+void restore_context(int task_id)
+{
+    // Code to restore the context of the task with task_id
+}
+
+void scheduler()
+{
+    if (context_switch == ENABLE)
+    {
+        context_switch = DISABLE;
+        save_context();  // Save current task context
+        current_task = (current_task + 1) % NUM_TASKS;  // Round-robin: move to the next task
+        restore_context(current_task);  // Restore next task context
+    }
+}
+
+void system_init()
+{
+    // System initialization code here
+}
+
+void config_systick()
+{
+    // SysTick configuration code here
+}
+
+void configure_exceptions()
+{
+    // Other exception configurations here
+}
+
+int main()
+{
+    system_init();
+    config_systick();
+    configure_exceptions();
+
+    while (TRUE)
+    {
+        scheduler();
+    }
+
+    return 0;
+}
